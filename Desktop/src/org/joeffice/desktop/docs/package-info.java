f686f4941046fe7cdf4399e8e0028ee613e6525f
Gebruiker <Gebruiker@Gebruiker-PC>
2013-02-24 13:45:08 +0100

    Added first version of desktop module.

---
+++ Desktop/src/org/joeffice/desktop/docs/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@HelpSetRegistration(helpSet = "desktop-hs.xml", position = 3838)
+package org.joeffice.desktop.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;
