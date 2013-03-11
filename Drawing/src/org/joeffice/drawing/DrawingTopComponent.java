372d608e70597fe728bd63bcf9be453feadbda60
Gebruiker <Gebruiker@Gebruiker-PC>
2013-03-11 23:47:01 +0100

    Added Apache license on top Save file location (so that it's reopened at start up)

--- Drawing/src/org/joeffice/drawing/DrawingTopComponent.java
+++ Drawing/src/org/joeffice/drawing/DrawingTopComponent.java
@@ -1,11 +1,23 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2013 Japplis.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.joeffice.drawing;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.util.Properties;
 import javax.swing.JComponent;
 
 import org.apache.batik.swing.JSVGCanvas;
@@ -98,15 +110,13 @@ public final class DrawingTopComponent extends OfficeTopComponent {
         }
     }
 
-    public void writeProperties(java.util.Properties p) {
-        // better to version settings since initial version as advocated at
-        // http://wiki.apidesign.org/wiki/PropertyFiles
-        p.setProperty("version", "1.0");
-        // TODO store your settings
+    @Override
+    public void writeProperties(Properties properties) {
+        super.writeProperties(properties);
     }
 
-    public void readProperties(java.util.Properties p) {
-        String version = p.getProperty("version");
-        // TODO read your settings according to their version
+    @Override
+    public void readProperties(Properties properties) {
+        super.readProperties(properties);
     }
 }
