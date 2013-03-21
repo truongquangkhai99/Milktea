95bd3235ec613bfe7270845a2f2ccadbe85d1da7
Gebruiker <Gebruiker@Gebruiker-PC>
2013-03-21 01:02:41 +0100

    Set position for template

--- wordprocessor/src/org/joeffice/wordprocessor/package-info.java
+++ wordprocessor/src/org/joeffice/wordprocessor/package-info.java
@@ -1,4 +1,4 @@
-@TemplateRegistration(folder = "Other", content = "DocxTemplate.docx")
+@TemplateRegistration(folder = "Other", content = "DocxTemplate.docx", position = 100)
 package org.joeffice.wordprocessor;
 
 import org.netbeans.api.templates.TemplateRegistration;
